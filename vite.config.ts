e435b1555314f78974751ab754e1b54ed43e5058
Rafael Niebles <rnieblesealo@gmail.com>
2025-01-23 07:05:34 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const base = "portfolio-website"
+
 // https://vite.dev/config/
 export default defineConfig(() => {
   const config = {
     plugins: [react()],
+    base: `/${base}/`
   }
 
   return config;
